6e47ed227122b74f25ea944e0b91cfe2281b7306
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2024-03-29 13:01:20 +0100

    initial UI implementation

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
-import BpmnViewer from 'bpmn-js';
+import BpmnModeler from 'bpmn-js';
 
-const piazzaDiagramViewer ='<?xml version="1.0" encoding="UTF-8"?><bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:camunda="http://camunda.org/schema/1.0/bpmn" id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn" exporter="Camunda Modeler" exporterVersion="4.0.0"><bpmn:process id="Process_1" isExecutable="true"><bpmn:startEvent id="StartEvent_1"/><bpmn:sequenceFlow id="Flow_1" sourceRef="StartEvent_1" targetRef="Task_1"/><bpmn:task id="Task_1" name="Task 1"><bpmn:outgoing>Flow_2</bpmn:outgoing></bpmn:task><bpmn:sequenceFlow id="Flow_2" sourceRef="Task_1" targetRef="EndEvent_1"/><bpmn:endEvent id="EndEvent_1"/></bpmn:process></bpmn:definitions>';
+import diagram from '../resources/diagram.bpmn';
 
-var viewer = new BpmnViewer({
+var viewer = new BpmnJS({
   container: '#canvas'
 });
 
-var modeler = new BpmnJS({
-  container: '#canvas' 
-});
+try {
 
-viewer.importXML(piazzaDiagramViewer).then(function(result) {
+  viewer.importXML(diagram);
 
-  const { warnings } = result;
-
-  console.log('success !', warnings);
-
+  console.log('success!');
   viewer.get('canvas').zoom('fit-viewport');
-}).catch(function(err) {
+} catch (err) {
 
-  const { warnings, message } = err;
-
-  console.log('something went wrong:', warnings, message);
-});
+  console.error('something went wrong:', err);
+}
