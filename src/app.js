b0f64eefe76e1e6e9b8ef03d43f798fac28689c0
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2024-03-30 19:57:35 +0100

    popup

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const import_button=document.getElementById('import_button');
 
 try {
   viewer.importXML(diagram);
-
   console.log('success!');
   viewer.get('canvas').zoom('fit-viewport');
 } catch (err) {
@@ -23,18 +22,82 @@ try {
 
 
 export_button.addEventListener('click', function () {
-  viewer.saveXML({ format: true, preamble: false })
-      .then(({ xml, error }) => {
-          if (error) {
-              console.log(error);
-          } else {
-              download(xml, 'diagram.bpmn', 'text/xml');
-          }
-      })
-      .catch(error => {
-          console.log(error);
-      });
+
+  var container = document.getElementById('canvas');
+
+  var background_container = document.createElement('div');
+  background_container.classList="background-container";
+
+  var popup = document.createElement('div');
+  popup.classList.add('popup');
+
+  var what_is=document.createElement('div');
+  what_is.innerHTML="<strong>Insert a title for the diagram</strong>";
+  what_is.style.marginLeft="20%"
+
+
+  popup.appendChild(what_is);
+
+  var inputText = document.createElement('input');
+  inputText.setAttribute('type', 'text');
+  inputText.setAttribute('placeholder', 'Insert the title');
+  inputText.setAttribute('value', 'Diagram');
+  inputText.style.marginTop="1.8vh";
+
+
+  popup.appendChild(inputText);
+
+  var confirmBtn = document.createElement('button');
+  confirmBtn.classList.add('btn-popup');
+  confirmBtn.style.backgroundColor = '#10ad74';
+  confirmBtn.style.textSize='1vh';
+  confirmBtn.textContent = 'Confirm and export';
+
+  popup.appendChild(confirmBtn);
+
+  var closeBtn = document.createElement('span');
+    closeBtn.classList.add('close-btn');
+    closeBtn.textContent = 'X';
+    closeBtn.style.marginBottom="0.4vh";
+  popup.appendChild(closeBtn);
+
+    
+  background_container.appendChild(popup);  
+  container.appendChild(background_container);
+
+
+  background_container.addEventListener('click', function(event) {
+    const target_=event.target
+    if(target_.className=="background-container"){
+      container.removeChild(background_container);
+    }
+  });
+  closeBtn.addEventListener('click', function() {
+    event.stopPropagation();
+    container.removeChild(background_container); 
+  });
+  confirmBtn.addEventListener('click', function() {
+    event.stopPropagation();
+    var title = inputText.value;
+    exportDiagram(title);    
+    container.removeChild(background_container); 
+  });
+
 });
+
+function exportDiagram(title){
+  viewer.saveXML({ format: true, preamble: false })
+  .then(({ xml, error }) => {
+      if (error) {
+          console.log(error);
+      } else {
+          download(xml, title + '.bpmn', 'text/xml');
+      }
+  })
+  .catch(error => {
+      console.log(error);
+  });
+}
 
 export_button.addEventListener('mouseover', () =>{
   document.getElementById("label_export_button").innerHtml = "Export";
